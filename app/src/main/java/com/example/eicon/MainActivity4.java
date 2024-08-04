1eb49f6bbc7ce46dbafd67239ba1b436d5ffa0df
정상미 <w2321@e-mirim.hs.kr>
2024-08-05 00:50:44 +0900

    11 페이지 완성, 12 페이지 작업, 이미지 업로드

--- app/src/main/java/com/example/eicon/MainActivity4.java
+++ app/src/main/java/com/example/eicon/MainActivity4.java
@@ -1,9 +1,6 @@
 package com.example.eicon;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
-
-    private ImageView image1;
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
-
-        image1 = findViewById(R.id.image1);
-        image1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_main4);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
